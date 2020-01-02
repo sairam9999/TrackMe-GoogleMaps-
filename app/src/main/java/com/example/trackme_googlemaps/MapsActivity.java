2b0f311da89e26e08de8f0e91e91438b87988a9a
sairam9999 <vsairamanroid@gmail.com>
2020-01-02 15:14:40 -0500

    Minor Update in the variable

--- app/src/main/java/com/example/trackme_googlemaps/MapsActivity.java
+++ app/src/main/java/com/example/trackme_googlemaps/MapsActivity.java
@@ -40,8 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(42.3140089, -71.2504676);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Boston"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng boston = new LatLng(42.3140089, -71.2504676);
+        mMap.addMarker(new MarkerOptions().position(boston).title("Marker in Boston"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(boston));
     }
 }
